a1f3bd2aeb4381a49150f2bfacf6db730b709e48
KengniJohan <kengnijordan9@gmail.com>
2026-01-30 11:08:38 +0100

    first commit

---
+++ egggo/src/main/java/com/example/egggo/EgggoApplication.java
@@ -0,0 +1,13 @@
+package com.example.egggo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgggoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgggoApplication.class, args);
+	}
+
+}
---
+++ egggo_mobile/android/app/src/main/kotlin/com/example/egggo_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.egggo_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
